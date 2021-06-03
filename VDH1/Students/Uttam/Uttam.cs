2058512af0c6f20f857e8171be56e84a4a6ab2ce
Uttam83 <uprety.uttam@gmail.com>
2021-06-03 10:51:07 +0200

    s

---
+++ VDH1/Students/Uttam/Uttam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDH1.Students.Uttam
+{
+    class Uttam
+    {
+        public string  Fullname { set => Fullname = "Uttam U."; }
+    }
+}
